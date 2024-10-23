40f74d3ea5e58951c0545abf1a000fa8376fedaa
juger1 <gajehaj8@gmail.com>
2024-10-23 12:51:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Activate the virtual environment
+source /app/venv/bin/activate
+
+# Run Flask and other processes
 flask run -h 0.0.0.0 -p 8000 &
 
 python3 ping_server.py &
 
 python3 worker.py
 
+# Wait for all background processes to finish
 wait
